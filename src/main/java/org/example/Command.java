a20aaa38964b30b2a9f70f6f389ad9f01910130e
naYmnichax <matus.matvey@yandex.ru>
2022-11-28 23:40:03 +0500

    first our commit

---
+++ src/main/java/org/example/Command.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Command {
+    public String SetCommand(String textCommand, String textUser, String name) {
+        Map<String, String> command = new HashMap<String, String>();
+        command.put("/start", Start(name));
+        command.put("/about", About());
+        command.put("/help", Help());
+        command.put("/echo", Echo(textUser));
+
+        for (String instruction : command.keySet()) {
+            if (instruction.equals(textCommand)) {
+                return command.get(instruction);
+            }
+        }
+        return "There is no such command. Make sure the command is written correctly or use the /help command.";
+    }
+
+    private static String Start(String username) {
+        return "Hello, " + username + ".\nWrite /help to find out the available bot commands";
+    }
+
+    private static String About() {
+        return "It was created by Matvey Matus";
+    }
+
+    private static String Help() {
+        return """
+                /about - Will tell you about who wrote it.
+                /echo - After the command, specify the sentence/phrase that you want to duplicate, separating them with a space.
+                /help - Write /help to find out the available bot commands.
+                """;
+    }
+
+    private static String Echo(String msg) {
+        if (msg.equals("")){
+            return "No message has been entered. Use the /help command.";
+        }
+        return "You sent: " + msg;
+    }
+}
---
+++ src/main/java/org/example/InfoBot.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class InfoBot {
+    public String getInfo(String fileName) {
+        File file = new File(fileName);
+        String line = null;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            line = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return line;
+    }
+}
